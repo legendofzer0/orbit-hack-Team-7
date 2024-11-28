2e9c825c6da9fa1ef199ba544508717b0e483b23
anup <aniruddharai39@gmail.com>
2024-11-29 02:00:45 +0545

    change

--- data_train_hardware.py
+++ data_train_hardware.py
@@ -16,14 +16,35 @@ class IntentManager:
 def load_data():
     manager = IntentManager()
     
+    # Adding intents and diverse phrases
     manager.add_intent("on the fan", [
-        "",
-        
+        "turn on the fan", "fan on", "start the fan",
+        "activate the fan", "enable the fan",
+        "switch on the fan", "power on the fan"
     ])
     
     manager.add_intent("off the fan", [
-        "",
-        
+        "turn off the fan", "fan off", "stop the fan",
+        "deactivate the fan", "disable the fan",
+        "switch off the fan", "power off the fan"
     ])
-
+    
+    manager.add_intent("on the light", [
+        "turn on the light", "light on", "start the light",
+        "activate the light", "enable the light",
+        "switch on the light", "power on the light"
+    ])
+    
+    manager.add_intent("off the light", [
+        "turn off the light", "light off", "stop the light",
+        "deactivate the light", "disable the light",
+        "switch off the light", "power off the light"
+    ])
+    
+    manager.add_intent("motion sensor", [
+        "check the room", "activate motion sensor",
+        "start motion detection", "detect motion",
+        "motion sensor on", "turn on motion sensor"
+    ])
+    
     return manager.get_data()
---
+++ demo.py
@@ -0,0 +1,9 @@
+user = int(input("Enter the number: "))
+if user == 1:
+    print("Welcome")
+    while True:
+        user_input = input("Enter your name: ")
+elif user == 2:
+    print("Goodbye")
+            
+       
