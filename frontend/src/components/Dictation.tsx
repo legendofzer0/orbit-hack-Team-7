7e2b348e0894055b72e036aeb30866d9dbe4064c
legendofzer0 <98453046+legendofzer0@users.noreply.github.com>
2024-11-30 02:49:40 +0545

    added select

--- frontend/src/components/Dictation.tsx
+++ frontend/src/components/Dictation.tsx
@@ -9,9 +9,11 @@ import "../css/mic.css";
 const Dictation = () => {
   const { transcript, resetTranscript, browserSupportsSpeechRecognition } =
     useSpeechRecognition();
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   const [speak, setSpeech] = useState();
+  const [select, setSelect] = useState("");
   const [isRecording, setIsRecording] = useState(false);
+
   useEffect(() => {
     setSpeech(transcript);
     console.log(speak);
@@ -21,10 +23,18 @@ const Dictation = () => {
       const getToken = localStorage.getItem("token");
       const getData = await getTokenData(getToken);
       setUser(getData);
-      setUser(getData);
     };
     gettokendataa();
   }, []);
+  useEffect(() => {
+    if (user) {
+      webSocketService.sendMessage({
+        userId: user.id,
+        userName: user.name,
+        content: select,
+      });
+    }
+  }, [select, user]);
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
@@ -62,6 +72,32 @@ const Dictation = () => {
 
   return (
     <div>
+      <div>
+        <input
+          type="radio"
+          name="select"
+          value="1"
+          checked={select === "1"}
+          onChange={() => setSelect("1")}
+        />
+        Chat Bot
+        <input
+          type="radio"
+          name="select"
+          value="2"
+          checked={select === "2"}
+          onChange={() => setSelect("2")}
+        />
+        Smart Home
+        <input
+          type="radio"
+          name="select"
+          value="3"
+          checked={select === "3"}
+          onChange={() => setSelect("3")}
+        />
+        Weather
+      </div>
       <span id="btn-back">
         <button
           id="button-mic"
